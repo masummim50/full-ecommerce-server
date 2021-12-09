6422f747afb577a078e8ce1f4158c1dffce85b00
unknown <masummim50@gmail.com>
2021-12-09 18:15:55 +0600

    style

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -8,20 +8,43 @@ const path = require('path')
 const { MongoClient } = require('mongodb');
 const { ObjectID } = require('mongodb');
 // const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://masumdb:passwordforatlas@cluster0.msj15.mongodb.net/ShoppingMall?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.msj15.mongodb.net/ShoppingMall?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.json());
 app.use(cors());
-
+// below line is there so i can link the css file properly. 
+app.use(express.static(path.join(__dirname, '/public')));
 
 client.connect(err => {
   // const collection = client.db("ShoppingMall").collection("allProducts");
  const all = client.db("ShoppingMall").collection("allProducts");
+ const newcollection = client.db('ShoppingMall').collection('newcollection');
+ 
   // perform actions on the collection object
   app.get('/allProducts', (req,res)=>{
     all.find().limit(20).toArray((err, documents)=>{
-      console.log(err)
+      res.send(documents)
+    })
+  })
+
+  app.get('/product/:id', (req, res)=> {
+    let id = req.params.id;
+    all.find({_id:ObjectID(`${id}`)}).toArray((err, documents)=> {
+      res.send(documents[0])
+    })
+  })
+
+  app.get('/category/:categoryType', (req, res)=> {
+    all.find({category:`${req.params.categoryType}`})
+    .toArray((err, documents)=> {
+      res.send(documents)
+    })
+  })
+
+  app.get('/seller/:brandName', (req, res)=> {
+    let brand = req.params.brandName;
+    all.find({seller:brand}).toArray((err, documents)=> {
       res.send(documents)
     })
   })
@@ -50,6 +73,6 @@ client.connect(err => {
 
 
 app.get('/', (req, res)=>{
-  res.sendFile(path.join(__dirname + '/main.html'))
+  res.sendFile(path.join(__dirname + '/public/views/main.html'))
 });
 app.listen(5000, ()=>console.log('successfully running'));
---
+++ public/views/main.js
@@ -0,0 +1,9 @@
+function copyToClipboard(id) {
+    var from = document.getElementById(id);
+    var range = document.createRange();
+    window.getSelection().removeAllRanges();
+    range.selectNode(from);
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+}
